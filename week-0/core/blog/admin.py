24d9bb513226db0c9e6574969274520c930ddfa2
RezaNikmanesh <rezanikmanesh79@gmail.com>
2025-07-12 12:30:27 +0330

    macking admin panel

--- week-0/core/blog/admin.py
+++ week-0/core/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    # this ordering will not change data base
+    ordering = ['-publish']
+    list_filter = ('status', 'author')
+    search_fields = ('title', 'content')
+    # add user id instead of name
+    raw_id_fields = ('author', )
+    # archiving post by date
+    date_hierarchy = 'publish'
+    # automating naming of slug
+    prepopulated_fields = {'slug': ('title',)}
+    # editing status on main page
+    list_editable = ('status',)
+    list_display_links = ['title', 'author']
--- week-0/core/blog/models.py
+++ week-0/core/blog/models.py
@@ -4,13 +4,12 @@ from django.contrib.auth.models import User
 
 
 class Post (models.Model):
-    
-    
+
     class Status(models.TextChoices):
         # code /data base / show user
         DRAFT = 'DF', 'draft'
         PUBLISHED = 'PB', 'published'
-        REJECTED = 'RJ','rejected'
+        REJECTED = 'RJ', 'rejected'
 
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.DRAFT)
     title = models.CharField(max_length=50)
@@ -19,14 +18,14 @@ class Post (models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     publish = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User,on_delete=models.CASCADE, related_name='user_posts')
-    
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
+
     def __str__(self):
         return self.title
-    
+
     # tune the class
     class Meta:
+        # this ordering change data base
         ordering = ['-publish']
         # indexing work in django model but have downfall of cpu proses
         indexes = [models.Index(fields=['publish'])]
