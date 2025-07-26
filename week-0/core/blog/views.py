be5202ac20ea982a8d7a8a1ffe62d7dd39377d67
Reza Nikmanesh <rezanikmanesh79@gmail.com>
2025-07-26 09:35:27 +0330

    adding get or 404

--- week-0/core/blog/views.py
+++ week-0/core/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post
 
@@ -10,7 +10,15 @@ def post_list(request):
     posts = Post.published.all()
     context={'posts':posts}
     return render(request, template_name='blog/post-list.html', context=context)
+
 def post_detail(request, pk):
-    post=Post.published.get(pk=pk)
+    # try:
+    #     post=Post.published.get(pk=pk)
+    #     context = {"post":post}
+    #     return render(request, template_name='blog/post-detail.html', context=context)
+    # except:
+    #     return HttpResponse('post dose not exist')
+    
+    post=get_object_or_404(Post, pk=pk)
     context = {"post":post}
     return render(request, template_name='blog/post-detail.html', context=context)
