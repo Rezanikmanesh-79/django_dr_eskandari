ee2b73a29c1b304a420c5d4ddca258e49325b6a9
Reza Nikmanesh <rezanikmanesh79@gmail.com>
2025-08-02 10:53:22 +0330

    twinking some html

--- week-0/core/blog/views.py
+++ week-0/core/blog/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post
 import datetime
-
+from django.core.paginator import Paginator , EmptyPage ,PageNotAnInteger
 def index(request):
     return HttpResponse("hello ")
 
 def post_list(request):
     posts = Post.published.all()
-    context={'posts':posts}
-    return render(request, template_name='blog/post-list.html', context=context)
+    paginator = Paginator(posts, per_page=2)
+    page_number = request.GET.get('page')
 
+    try:
+        page_obj = paginator.page(page_number)
+    # if user input some ting is not int
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    context = {'posts': page_obj}
+    return render(request, 'blog/post-list.html', context)
 def post_detail(request, pk):
     # try:
     #     post=Post.published.get(pk=pk)
