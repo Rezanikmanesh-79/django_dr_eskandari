2bd6f4ea7f773253b1d336c5750732d165ca93f3
Reza Nikmanesh <rezanikmanesh79@gmail.com>
2025-08-23 12:01:03 +0330

    conect postgre data

--- week-0/core/core/settings.py
+++ week-0/core/core/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shinigami_db',
+        'USER': 'shinigami',            
+        'PASSWORD': 'root',        
+        'HOST': 'localhost',             
+        'PORT': '5432',                 
     }
 }
 
